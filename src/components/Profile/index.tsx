c4d345f168ecf751d90edeeb10bbb758f7e14964
Anna <missjellx@gmail.com>
2022-11-19 20:28:26 +0500

    add css modules

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,22 +1,24 @@
+import styles from "./style.module.css";
+
 export const Profile = () => {
   return (
-    <div className="content">
+    <div className={styles.content}>
       <img
         src="https://png.pngtree.com/thumb_back/fh260/background/20200714/pngtree-modern-double-color-futuristic-neon-background-image_351866.jpg"
         alt="user-bg"
       />
-      <div className="user-info">
+      <div className={styles["user-info"]}>
         <img
           src="https://upload.wikimedia.org/wikipedia/commons/7/77/Avatar_cat.png"
           alt="user avatar"
         />
-        <div className="user-details">user details</div>
+        <div>user details</div>
       </div>
-      <div className="posts-container">
-        <div className="create-post-container">create post</div>
-        <div className="posts">
-          <div className="post">post 1</div>
-          <div className="post">post 2</div>
+      <div>
+        <div>create post</div>
+        <div>
+          <div></div>
+          <div>post 2</div>
         </div>
       </div>
     </div>
