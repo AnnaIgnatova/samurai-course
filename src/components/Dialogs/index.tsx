10bb66b8ebf23a23beebdb2ad1ad49a1ee9786d0
Anna <missjellx@gmail.com>
2022-11-22 21:58:03 +0500

    add dialogs page

---
+++ src/components/Dialogs/index.tsx
@@ -0,0 +1,5 @@
+import styles from "./style.module.css";
+
+export const Dialogs = () => {
+  return <div>dialogs</div>;
+};
--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -3,13 +3,17 @@ import styles from "./style.module.css";
 
 export const Profile: React.FC = () => {
   return (
-    <div className={styles.content}>
-      <img src="./assets/bg-profile.jpg" alt="user-bg" />
+    <>
+      <img
+        src="./assets/bg-profile.jpg"
+        alt="user-bg"
+        className={styles["profile-img"]}
+      />
       <div className={styles["user-info"]}>
         <img src="./assets/avatar.png" alt="user avatar" />
         <div className={styles["user-details"]}>Name</div>
       </div>
       <Posts />
-    </div>
+    </>
   );
 };
