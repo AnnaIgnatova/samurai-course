291babc7c4c04bb23477a15bb4c61dd6890b3fbe
Anna <missjellx@gmail.com>
2022-11-23 23:01:44 +0500

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Dialogs } from "./components/Dialogs";
 import { Header } from "./components/Header";
@@ -6,14 +7,18 @@ import { Profile } from "./components/Profile";
 
 const App: React.FC = () => {
   return (
-    <div className="container">
-      <Header />
-      <Navbar />
-      <div className="content">
-        {/* <Profile /> */}
-        <Dialogs />
+    <BrowserRouter>
+      <div className="container">
+        <Header />
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/dialogs" element={<Dialogs />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 };
 
