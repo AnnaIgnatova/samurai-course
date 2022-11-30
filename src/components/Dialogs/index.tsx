6a9ee7ebcd45092dc33bae2c4a016e5725d7945b
Anna <missjellx@gmail.com>
2022-11-30 22:44:37 +0500

    add dialogs page

--- src/components/Dialogs/index.tsx
+++ src/components/Dialogs/index.tsx
@@ -1,6 +1,20 @@
 import styles from "./style.module.css";
 
 export const Dialogs = () => {
-    return <div className={styles.container}>dialogs</div>;
-  };
-  
+  return (
+    <div className={styles.container}>
+      <div className={styles["dialogs-items"]}>
+        <div className={styles.dialog}>Anna</div>
+        <div className={styles.dialog}>Sasha</div>
+        <div className={styles.dialog}>Olya</div>
+        <div className={styles.dialog}>Kirill</div>
+        <div className={styles.dialog}>Dima</div>
+      </div>
+      <div className={styles["messages"]}>
+        <div className={styles.message}>Hi</div>
+        <div className={styles.message}>Good morning</div>
+        <div className={styles.message}>How are you</div>
+      </div>
+    </div>
+  );
+};
