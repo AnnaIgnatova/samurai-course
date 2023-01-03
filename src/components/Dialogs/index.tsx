730225f5b5e88a69bc187187a426b6c717adf44a
Anna <missjellx@gmail.com>
2023-01-04 00:42:13 +0500

    replace dialog and profile data to constants

--- src/components/Dialogs/index.tsx
+++ src/components/Dialogs/index.tsx
@@ -3,19 +3,56 @@ import { Message } from "./Message";
 import styles from "./style.module.css";
 
 export const Dialogs = () => {
+  const dialogsData = [
+    {
+      id: 1,
+      name: "Anna",
+    },
+    {
+      id: 2,
+      name: "Sasha",
+    },
+    {
+      id: 3,
+      name: "Olya",
+    },
+    {
+      id: 4,
+      name: "Misha",
+    },
+    {
+      id: 5,
+      name: "Dima",
+    },
+  ];
+
+  const messagesData = [
+    {
+      id: 1,
+      text: "Hi",
+    },
+    {
+      id: 2,
+      text: "Good morning",
+    },
+    {
+      id: 3,
+      text: "How are you",
+    },
+  ];
   return (
     <div className={styles.container}>
       <div className={styles["dialogs-items"]}>
-        <Dialog id={1} name="Anna" />
-        <Dialog id={2} name="Sasha" />
-        <Dialog id={3} name="Olya" />
-        <Dialog id={4} name="Misha" />
-        <Dialog id={5} name="Dima" />
+        <Dialog id={dialogsData[0].id} name={dialogsData[0].name} />
+        <Dialog id={dialogsData[1].id} name={dialogsData[1].name} />
+        <Dialog id={dialogsData[2].id} name={dialogsData[2].name} />
+        <Dialog id={dialogsData[3].id} name={dialogsData[3].name} />
+        <Dialog id={dialogsData[4].id} name={dialogsData[4].name} />
       </div>
       <div className={styles["messages"]}>
-        <Message text="Hi" />
-        <Message text="Good morning" />
-        <Message text="How are you" />
+        <Message text={messagesData[0].text} />
+        <Message text={messagesData[1].text} />
+        <Message text={messagesData[2].text} />
       </div>
     </div>
   );
