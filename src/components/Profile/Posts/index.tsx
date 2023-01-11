511769f70a7b103b1d8bda7351703f4b458ddf1b
Anna <missjellx@gmail.com>
2023-01-11 23:23:04 +0500

    add create post function

--- src/components/Profile/Posts/index.tsx
+++ src/components/Profile/Posts/index.tsx
@@ -1,13 +1,21 @@
+import React, { RefObject } from "react";
 import { PostsData } from "../../../interfaces";
 import { Post } from "./Post";
 import styles from "./style.module.css";
 
 export const Posts: React.FC<PostsData> = ({ posts }) => {
+  const postField = React.useRef<HTMLTextAreaElement>(null);
+
+  const createPost = () => {
+    const text = postField.current && postField.current.value;
+    console.log(text);
+  };
+
   return (
     <div className={styles["posts-container"]}>
       <div className={styles["create-post"]}>
-        <textarea placeholder="type something"></textarea>
-        <button>Add post</button>
+        <textarea placeholder="type something" ref={postField} ></textarea>
+        <button onClick={createPost}>Add post</button>
       </div>
       <div>
         {posts.map(({ text, likes }) => (
