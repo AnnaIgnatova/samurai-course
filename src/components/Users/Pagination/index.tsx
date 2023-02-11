33c57b9a1eeffa2dbb97e25cc58604bc79b782bb
Anna <missjellx@gmail.com>
2023-02-11 23:31:47 +0500

    add styles to users page

---
+++ src/components/Users/Pagination/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+export interface PagintaionData {
+  currentPage: number;
+  handlePage: (page: number) => void;
+}
+
+export const Pagination: React.FC<PagintaionData> = ({
+  currentPage,
+  handlePage,
+}) => {
+  return (
+    <div className={styles.pages}>
+      {Array.from(Array(10).keys()).map((page) => (
+        <div
+          className={`${styles.page} ${
+            currentPage === page + 1 && styles["active-page"]
+          }`}
+          onClick={() => handlePage(page + 1)}
+        >
+          {page + 1}
+        </div>
+      ))}
+    </div>
+  );
+};
