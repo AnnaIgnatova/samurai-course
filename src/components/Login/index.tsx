7ec3beadf2eb74be15778113e7b85b621bd16f4c
Anna <missjellx@gmail.com>
2023-02-14 23:32:48 +0500

    add login page

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,7 +1,44 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
 
 import "./style.module.css";
 
+export interface LoginFormData {
+  handleSubmit: any;
+}
+
 export const Login: React.FC = () => {
-  return <div>login page</div>;
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+  return (
+    <div>
+      login page
+      <LoginReduxForm handleSubmit={onSubmit} />
+    </div>
+  );
 };
+
+export const LoginForm: React.FC<LoginFormData> = ({ handleSubmit }) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="email">Email</label>
+        <Field name="email" component="input" type="email" />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <Field name="password" component="input" type="password" />
+      </div>
+      <div>
+        <label htmlFor="accept_data">Accept</label>
+        <Field name="accept_data" component="input" type="checkbox" />
+      </div>
+      <button type="submit">Log in</button>
+    </form>
+  );
+};
+
+export const LoginReduxForm = reduxForm({
+  form: "login",
+})(LoginForm);
