0e35aee5d6dcb977303555344d0532879bc34409
Anna <missjellx@gmail.com>
2023-02-28 21:48:40 +0500

    add tests for profile status

--- src/components/Profile/__tests__/Profile.test.tsx
+++ src/components/Profile/__tests__/Profile.test.tsx
@@ -1,18 +1,19 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import { Status } from "../Status";
 import { create } from "react-test-renderer";
 
-test("render profile status", () => {
-  const view = render(<Status text={""} updateStatus={() => {}} />);
-//   const root = component.root;
-//   const span = root.findByType("span");
-//   console.log(view);
-  expect(view).toHaveAttribute("text", "");
+test("profile status text", () => {
+  const component = create(
+    <Status text={"new text"} updateStatus={() => {}} />
+  );
+  const root = component.root;
+  const span = root.findByType("span");
+  expect(span._fiber.stateNode.props.children).toBe("new text");
 });
 
-// test("status no active mode", () => {
-//   const component = create(<Status text={""} updateStatus={() => {}} />);
-//   const root = component.root;
-//   const span = root.findByType("span");
-//   expect(span).not.toBeNull();
-// });
+test("status no active mode", () => {
+  const component = create(<Status text={""} updateStatus={() => {}} />);
+  const root = component.root;
+  const span = root.findByType("span");
+  expect(span).not.toBeNull();
+});
