57a2e5d116ab1c6b5ce51c21f8c93b53a0030acd
Anna <missjellx@gmail.com>
2023-02-28 22:58:24 +0500

    add react lazy func

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,21 @@ import { Music } from "./components/Music";
 import { News } from "./components/News";
 import { Settings } from "./components/Settings";
 import "./App.css";
-import DialogsContainer from "./containers/DialogsContainer";
 import { NavbarContainer } from "./containers/NavbarContainer";
 import UsersContainer from "./containers/UsersContainer";
-import ProfileContainer from "./containers/ProfileContainer";
 import { AuthContainer } from "./containers/AuthContainer";
 import { LoginContainer } from "./containers/LoginContainer";
+import { Suspense } from "react";
+import { Loader } from "./components/UI/Loader";
+import React from "react";
+
+const ProfileContainer = React.lazy(
+  () => import("./containers/ProfileContainer")
+);
+
+const DialogsContainer = React.lazy(
+  () => import("./containers/DialogsContainer")
+);
 
 const App: React.FC<any> = () => {
   return (
@@ -16,15 +25,17 @@ const App: React.FC<any> = () => {
       <AuthContainer />
       <NavbarContainer />
       <div className="content">
-        <Routes>
-          <Route path="/profile/:id" element={<ProfileContainer />} />
-          <Route path="/dialogs/*" element={<DialogsContainer />} />
-          <Route path="/news" element={<News />} />
-          <Route path="/music" element={<Music />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="/users" element={<UsersContainer />} />
-          <Route path="/login" element={<LoginContainer />} />
-        </Routes>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/profile/:id" element={<ProfileContainer />} />
+            <Route path="/dialogs/*" element={<DialogsContainer />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/music" element={<Music />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/users" element={<UsersContainer />} />
+            <Route path="/login" element={<LoginContainer />} />
+          </Routes>
+        </Suspense>
       </div>
     </div>
   );
