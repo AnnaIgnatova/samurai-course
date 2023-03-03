aed7006a6ecac364f74907299848fa7589ae393d
Anna <missjellx@gmail.com>
2023-03-04 00:23:51 +0500

    replace user details to separate component

--- src/components/Profile/ProfileInfo/index.tsx
+++ src/components/Profile/ProfileInfo/index.tsx
@@ -2,22 +2,12 @@ import { ProfileAPIData } from "../../../interfaces";
 import styles from "./style.module.css";
 import userImg from "./../../../assets/avatar.png";
 import profileBg from "./../../../assets/bg-profile.jpg";
-import { Status } from "../Status";
 
-export const ProfileInfo: React.FC<ProfileAPIData> = ({
-  profileData,
-  status,
-  ownProfile,
-  updateStatusDataThunk,
-  saveProfilePhotoThunk,
-}) => {
-  const {
-    photos,
-    lookingForAJob,
-    lookingForAJobDescription,
-    fullName,
-    contacts,
-  } = profileData;
+import { UserDetails } from "../UserDetails";
+
+export const ProfileInfo: React.FC<ProfileAPIData> = (props) => {
+  const { profileData, ownProfile, saveProfilePhotoThunk } = props;
+  const { photos, fullName } = profileData;
 
   const changePhoto = (e: any) => {
     const { target } = e;
@@ -32,22 +22,7 @@ export const ProfileInfo: React.FC<ProfileAPIData> = ({
           <img src={photos.small ? photos.small : userImg} alt={fullName} />
           {ownProfile && <input type="file" onChange={changePhoto} />}
         </div>
-        <div className={styles["details-container"]}>
-          <Status text={status} updateStatus={updateStatusDataThunk} />
-          <span className={styles["user-details"]}>{fullName}</span>
-          <span className={styles["user-details"]}>
-            looking for a job: {lookingForAJob ? "yes" : "no"}
-          </span>
-          <span className={styles["user-details"]}>
-            {lookingForAJobDescription}
-          </span>
-          <span className={styles["user-details"]}>vk: {contacts.vk}</span>
-          <span className={styles["user-details"]}>
-            inst: {contacts.instagram}
-          </span>
-          <span className={styles["user-details"]}>yt: {contacts.youtube}</span>
-          <button>edit data</button>
-        </div>
+        <UserDetails {...props} />
       </div>
     </>
   );
