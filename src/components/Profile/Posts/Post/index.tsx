f312db6d21ee88d0054b38fac58ba2fd3540de41
Anna <missjellx@gmail.com>
2023-03-08 19:11:41 +0500

    design posts

--- src/components/Profile/Posts/Post/index.tsx
+++ src/components/Profile/Posts/Post/index.tsx
@@ -1,19 +1,32 @@
 import { PostData } from "../../interfaces";
 import styles from "./style.module.css";
 import userAvatarUrl from "../../../../assets/user-avatar.png";
-import likeUrl from "../../../../assets/like.png";
+import likeUrl from "../../../../assets/like.svg";
+import commentUrl from "../../../../assets/comment.svg";
+import shareUrl from "../../../../assets/share.svg";
 
-export const Post: React.FC<any> = ({ message, likes, profileData }) => {
+export const Post: React.FC<any> = (props) => {
+  const { message, likes, profileData, comments, shares, pinned } = props;
   return (
     <div className={styles.post}>
-      <div className={styles["user-info"]}>
-        <img src={profileData.photos.large} alt="avatar" />
-        <span>{profileData.fullName}</span>
-      </div>
-      <div className={styles.content}>{message}</div>
-      <div className={styles.statistic}>
-        <img src={likeUrl} alt="like" />
-        {likes}
+      <img src={profileData.photos.large} alt="avatar" />
+      <div className={styles["post-content"]}>
+        <span className={styles["user-info"]}>{profileData.fullName}</span>
+        <span className={styles.content}>{message}</span>
+        <div className={styles["post-navbar"]}>
+          <div className={styles.statistic}>
+            <img src={likeUrl} alt="like" />
+            <span>{likes}</span>
+          </div>
+          <div className={styles.statistic}>
+            <img src={commentUrl} alt="comment" />
+            <span>{comments}</span>
+          </div>
+          <div className={styles.statistic}>
+            <img src={shareUrl} alt="share" />
+            <span>{shares}</span>
+          </div>
+        </div>
       </div>
     </div>
   );
