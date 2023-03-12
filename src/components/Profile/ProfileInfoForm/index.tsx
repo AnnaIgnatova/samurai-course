42cfbd387a853318df5f74084b1dfd9a1eb0d679
Anna <missjellx@gmail.com>
2023-03-12 22:31:30 +0500

    design edit user info form

--- src/components/Profile/ProfileInfoForm/index.tsx
+++ src/components/Profile/ProfileInfoForm/index.tsx
@@ -1,6 +1,8 @@
 import { Field, reduxForm } from "redux-form";
 import { FormInput } from "../../UI/Form/Field";
 
+import styles from "./style.module.css";
+
 export const ProfileInfoFormContainer: React.FC<any> = ({
   profile,
   toggleEditMode,
@@ -19,48 +21,68 @@ export const ProfileInfoForm: React.FC<any> = ({
   error,
 }) => {
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        User name
-        <Field
-          name="fullName"
-          component={FormInput}
-          type="text"
-          placeholder="Type Full name"
-        />
-      </label>
-      <label>
-        About me
-        <Field
-          name="aboutMe"
-          component={FormInput}
-          type="text"
-          placeholder="About me"
-        />
-      </label>
-      <label>
-        Is looking for a job
-        <Field name="lookingForAJob" component={FormInput} type="checkbox" />
-      </label>
-      <label>
-        Looking for a job description
-        <Field
-          name="lookingForAJobDescription"
-          component={FormInput}
-          type="text"
-          placeholder="Description"
-        />
-      </label>
-      {Object.entries(profile.contacts).map(([key, val]) => (
+    <form onSubmit={handleSubmit} className={styles.form}>
+      <h3>Edit user data</h3>
+      <div>
         <label>
-          {key}
+          User name
           <Field
-            name={key}
+            name="fullName"
             component={FormInput}
             type="text"
-            placeholder={key}
+            placeholder="Type Full name"
+            className={styles["form-input"]}
           />
         </label>
+      </div>
+      <div>
+        <label>
+          About me
+          <Field
+            name="aboutMe"
+            component={FormInput}
+            type="text"
+            placeholder="About me"
+            className={styles["form-input"]}
+          />
+        </label>
+      </div>
+      <div className={styles["form-checkbox-container"]}>
+        <label>
+          Is looking for a job
+          <Field
+            name="lookingForAJob"
+            component={FormInput}
+            type="checkbox"
+            className={styles["form-checkbox"]}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Looking for a job description
+          <Field
+            name="lookingForAJobDescription"
+            component={FormInput}
+            type="text"
+            placeholder="Description"
+            className={styles["form-input"]}
+          />
+        </label>
+      </div>
+      {Object.entries(profile.contacts).map(([key, val]) => (
+        <div>
+          <label>
+            {key}
+            <Field
+              name={key}
+              component={FormInput}
+              type="text"
+              placeholder={key}
+              className={styles["form-input"]}
+            />
+          </label>
+        </div>
       ))}
       {error && <span>{error}</span>}
       <button type="submit">Save</button>
