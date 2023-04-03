c8e0f91f7208cb62fd4623facf35baa0fb922faf
Anna <missjellx@gmail.com>
2023-04-03 23:07:17 +0500

    change edit profile data form to formik and refactor container components

--- src/MainApp.tsx
+++ src/MainApp.tsx
@@ -1,6 +1,6 @@
 import { store } from "./redux";
 import { Provider } from "react-redux";
-import AppContainer from "./containers/AppContainer";
+import { AppContainer } from "./containers/AppContainer";
 import { BrowserRouter } from "react-router-dom";
 
 export const MainApp = () => {
