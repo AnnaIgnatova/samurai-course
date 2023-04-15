dc33785b01233bdc9b864729b2b0843d16c313d9
Anna <missjellx@gmail.com>
2023-04-15 14:56:38 +0300

    reconnect websocket

--- src/components/Dialogs/Message/index.tsx
+++ src/components/Dialogs/Message/index.tsx
@@ -1,10 +1,18 @@
-import styles from "./style.module.css";
+import { Avatar } from 'antd';
+import { MessageData } from '..';
+import styles from './style.module.css';
 
-export interface MessageData {
-  text: string;
-  from: string;
-}
-
-export const Message: React.FC<MessageData> = ({ text, from }) => {
-  return <p className={styles[`from-${from}`]}>{text}</p>;
+export const Message: React.FC<MessageData> = ({
+  message,
+  photo,
+  userName,
+  userId,
+}) => {
+  return (
+    <p className={styles[`from-${userId === 27789 ? 'me' : 'them'}`]}>
+      {photo ? <Avatar src={photo} /> : <Avatar />}
+      <span>{userName}</span>
+      <span>{message}</span>
+    </p>
+  );
 };
